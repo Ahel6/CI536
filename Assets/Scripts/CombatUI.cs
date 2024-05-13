146e735c258a7f8f86d07eba8d17746d6b8f6cac
_nebula <41904486+misternebula@users.noreply.github.com>
2024-05-13 15:03:15 +0100

    some test combat stuff

--- Assets/Scripts/CombatUI.cs
+++ Assets/Scripts/CombatUI.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.EventSystems.EventTrigger;
 
 namespace Assets.Scripts
 {
@@ -14,6 +16,11 @@ namespace Assets.Scripts
 
 		public Image PlayerHealthbar;
 		public Image EnemyHealthbar;
+
+		public Text EnemyName;
+
+		public Text EventLog;
+		private string _eventLogString;
 
 		public Enemy Enemy;
 
@@ -25,12 +32,97 @@ namespace Assets.Scripts
 		public void LoadEnemy(Enemy enemy)
 		{
 			Enemy = enemy;
+			EnemyName.text = enemy.Name;
+
+			UpdateHealthbars();
+
+			WriteToEventLog($"- Encountered {enemy.Name}!");
+		}
+
+		public void PlayerAttackEnemy(Weapon weapon)
+		{
+			var damage = weapon.Damage;
+
+			WriteToEventLog($"- Player attacked {Enemy.Name} for {damage}!");
+		}
+
+		public void EnemyAttackPlayer(Weapon weapon)
+		{
+
 		}
 
 		public void UpdateHealthbars()
 		{
 			PlayerHealthbar.fillAmount = Player.Instance.Health / Player.Instance.MaxHealth;
+			PlayerHealthbar.GetComponentInChildren<Text>().text = Player.Instance.Health.ToString();
 			EnemyHealthbar.fillAmount = Enemy.CurrentHealth / Enemy.MaxHealth;
+			EnemyHealthbar.GetComponentInChildren<Text>().text = Enemy.CurrentHealth.ToString();
+		}
+
+		private const int LINE_COUNT = 14;
+		private const int CHAR_COUNT = 20;
+
+		private readonly ListStack<string> _messages = new(false);
+		private readonly string[] _lines = new string[LINE_COUNT];
+
+		public void WriteToEventLog(string message)
+		{
+			_messages.Push(message);
+
+			if (_messages.Count > LINE_COUNT)
+			{
+				_messages.PopFromBack();
+			}
+
+			var currentLineIndex = LINE_COUNT - 1;
+
+			foreach (var msg in _messages.Reverse())
+			{
+				var characterCount = msg.Length;
+				var linesNeeded = Mathf.CeilToInt((float)characterCount / CHAR_COUNT);
+				var chunk = 0;
+				for (var i = linesNeeded - 1; i >= 0; i--)
+				{
+					if (currentLineIndex - i < 0)
+					{
+						chunk++;
+						continue;
+					}
+
+					var chunkString = string.Concat(msg.Skip(CHAR_COUNT * chunk).Take(CHAR_COUNT));
+					_lines[currentLineIndex - i] = chunkString;
+					chunk++;
+				}
+
+				currentLineIndex -= linesNeeded;
+
+				if (currentLineIndex < 0)
+				{
+					break;
+				}
+			}
+
+			var finalText = "";
+			foreach (var line in _lines)
+			{
+				var msg = line;
+
+				if (line == default)
+				{
+					finalText += Environment.NewLine;
+				}
+				else if (msg.Length == CHAR_COUNT + 1)
+				{
+					finalText += msg;
+				}
+				else
+				{
+					finalText += $"{msg}{Environment.NewLine}";
+				}
+
+			}
+
+			EventLog.text = finalText;
 		}
 	}
 }
---
+++ Assets/Scripts/ListStack.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// A LIFO collection with List<> functionality.
+/// </summary>
+public class ListStack<T> : IEnumerable<T>
+{
+	private List<T> _items = new();
+
+	public int Count => _items.Count;
+
+	private readonly bool _removeDuplicates;
+
+	/// <param name="removeDuplicates">If true, all elements equal to the added item will be removed prior to adding the new element.</param>
+	public ListStack(bool removeDuplicates)
+	{
+		_removeDuplicates = removeDuplicates;
+	}
+
+	/// <summary>
+	/// Removes all items from the stack.
+	/// </summary>
+	public void Clear()
+		=> _items.Clear();
+
+	/// <summary>
+	/// Pushes an element onto the front of the stack.
+	/// </summary>
+	public void Push(T item)
+	{
+		if (_removeDuplicates && _items.Contains(item))
+		{
+			RemoveAll(x => EqualityComparer<T>.Default.Equals(x, item));
+		}
+
+		_items.Add(item);
+	}
+
+	/// <summary>
+	/// Pops an element off the front of the stack.
+	/// </summary>
+	public T PopFromFront()
+	{
+		if (_items.Count > 0)
+		{
+			var temp = _items[_items.Count - 1];
+			_items.RemoveAt(_items.Count - 1);
+			return temp;
+		}
+
+		return default;
+	}
+
+	/// <summary>
+	/// Pops an element off the back of the stack and shifts the entire stack backwards.
+	/// </summary>
+	public T PopFromBack()
+	{
+		if (_items.Count == 0)
+		{
+			return default;
+		}
+
+		var firstElement = _items[0];
+		_items.RemoveAt(0);
+		return firstElement;
+	}
+
+	/// <summary>
+	/// Returns the element at the front of the stack.
+	/// </summary>
+	public T PeekFront() => _items.Count > 0
+		? _items[_items.Count - 1]
+		: default;
+
+	/// <summary>
+	/// Returns the element at the back of the stack.
+	/// </summary>
+	public T PeekBack() => _items.Count > 0
+		? _items[0]
+		: default;
+
+	/// <summary>
+	/// Removes the element at the given index, where 0 is the back of the stack. The stack will shift backwards to fill empty space.
+	/// </summary>
+	public void RemoveAt(int index)
+		=> _items.RemoveAt(index);
+
+	/// <summary>
+	/// Removes the first occurence (back to front) of an item.
+	/// </summary>
+	public bool Remove(T item)
+		=> _items.Remove(item);
+
+	/// <summary>
+	/// Removes all elements that match the given predicate.
+	/// </summary>
+	public int RemoveAll(Predicate<T> match)
+		=> _items.RemoveAll(match);
+
+	/// <summary>
+	/// Returns the index of the given item, where 0 is the back of the stack.
+	/// </summary>
+	public int IndexOf(T item)
+		=> _items.IndexOf(item);
+
+	public T this[int index]
+	{
+		get => _items[index];
+		set => _items[index] = value;
+	}
+
+	IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>)_items).GetEnumerator();
+	public IEnumerator GetEnumerator() => ((IEnumerable<T>)_items).GetEnumerator();
+}
